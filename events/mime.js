47b3096cc3c0a83a791dd0ac7f771a140ecb948f
Sreelakshmi <nsreelakshmi2006@gmail.com>
2025-04-19 02:05:48 +0530

    first

---
+++ events/mime.js
@@ -0,0 +1,19 @@
+document.getElementById('toggleWinners').addEventListener('click', function () {
+    const winners = document.getElementById('winners');
+    winners.classList.toggle('hidden');
+    this.textContent = winners.classList.contains('hidden') ? 'Show Winners' : 'Hide Winners';
+  });
+  
+  function goBack() {
+    window.history.back();
+  }
+  document.getElementById('toggleWinners').addEventListener('click', function () {
+    const winners = document.getElementById('winners');
+    winners.classList.toggle('hidden');
+    this.textContent = winners.classList.contains('hidden') ? 'Show Winners' : 'Hide Winners';
+  });
+  
+  function goBack() {
+    window.history.back();
+  }
+  
